f8838a58255fa8372aa39736402d190c148d31ae
LukasJCode <89071785+LukasJCode@users.noreply.github.com>
2021-08-23 13:07:08 +0200

    Model classes added

---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+	public class MockEmployeeRepository : IEmployeeRepository
+	{
+		private List<Employee> _employeeList;
+
+		public MockEmployeeRepository()
+		{
+			_employeeList = new List<Employee>()
+			{
+				new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@gmail.com"},
+				new Employee() { Id = 2, Name = "John", Department = "IT", Email = "john@gmail.com" },
+				new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@gmail.com" }
+			};
+		}
+
+		public Employee GetEmployee(int Id)
+		{
+			//for (int i = 0; i < _employeeList.Count; i++)
+			//{
+			//	if (Id == _employeeList[i].Id)
+			//		return _employeeList[i];
+			//}
+			//return null;
+
+			return _employeeList.FirstOrDefault(e => e.Id == Id);
+		}
+	}
+}
--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace EmployeeManagement
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvcCore(options => options.EnableEndpointRouting = false);
+			services.AddMvc(options => options.EnableEndpointRouting = false);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
